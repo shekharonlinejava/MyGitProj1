754289897c0ecbe9175db218d4b979e7f77f19d3
shekharonlinejava <shekharonlinejava@gmail.com>
2025-07-11 08:42:01 +0530

    GitDemoProj committed

---
+++ GitDemoProj/src/main/java/com/rajtech/GitDemoProjApplication.java
@@ -0,0 +1,13 @@
+package com.rajtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoProjApplication.class, args);
+	}
+
+}
---
+++ GitDemoProj/src/test/java/com/rajtech/GitDemoProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
